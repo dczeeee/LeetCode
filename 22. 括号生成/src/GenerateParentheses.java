74d5da591eeb3211ce9b5d459984e0e4eba9ca2e
dczeeee <wanghaoyu349@126.com>
2019-02-12 20:13:45 +0800

    2/12

--- 22. 括号生成/src/GenerateParentheses.java
+++ 22. 括号生成/src/GenerateParentheses.java
@@ -1,4 +1,18 @@
+import java.util.List;
 
+/*
+ * 给出 n 代表生成括号的对数，请你写出一个函数，使其能够生成所有可能的并且有效的括号组合。
+
+例如，给出 n = 3，生成结果为：
+
+[
+  "((()))",
+  "(()())",
+  "(())()",
+  "()(())",
+  "()()()"
+]
+ */
 public class GenerateParentheses {
 
 	public static void main(String[] args) {
@@ -6,4 +20,7 @@ public class GenerateParentheses {
 
 	}
 
+	public List<String> generateParenthesis(int n) {
+        
+    }
 }
